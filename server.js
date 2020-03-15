d1afccce421ca62071ad3ed707ba306c3d310bb9
varunindoria1990 <varunindoria1990@gmail.com>
2020-03-15 21:41:59 +0530

    VI: Fix heroku issue 5

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 // server.js
 const express = require('express');
 const app = express();
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + '/dist'));
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+} else {    
+    // Run the app by serving the static files
+    // in the dist directory
+    app.use(express.static(__dirname + '/dist'));
+}
+
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
