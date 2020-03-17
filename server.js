531947ff1fa9bcf25584cad5864284b4072fda59
varunindoria1990 <varunindoria1990@gmail.com>
2020-03-17 07:33:56 +0530

    VI: Updated server.js file - 1

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
-// server.js
 const express = require('express');
 const app = express();
+const path = require('path');
+const port = process.env.PORT || 8000;
+const server = require('http').Server(app);
 
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
-} else {    
-    // Run the app by serving the static files
-    // in the dist directory
-    app.use(express.static(__dirname + '/dist'));
-}
+app.use(express.static(__dirname, 'dist', {index: false}));
 
-app.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+
+server.listen(port, function() {
+    console.log("App running on port " + port);
+})
+
+// PathLocationStrategy
+
+app.get('', function(req, res) {
+    res.sendFile(path.join(__dirname, 'src', 'index.html'));
 });
 
-// Start the app by listening on the default
-// Heroku port
-app.listen(process.env.PORT || 8080);
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'src', 'index.html'));
+});
